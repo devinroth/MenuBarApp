d523b5619b17457efcb90c918699cc20e6fda112
Devin Roth <devin@devinrothmusic.com>
2022-12-12 14:16:00 -0800

    Init

--- MenuApp/MenuAppApp.swift
+++ MenuApp/MenuAppApp.swift
@@ -9,9 +9,51 @@ import SwiftUI
 
 @main
 struct MenuAppApp: App {
+    
+    @StateObject var test = Test()
+    
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        MenuBarExtra("Title") {
+            VStack {
+                Toggle("Toggle", isOn: $test.toggle)
+
+                List {
+                    ForEach(test.beatles, id: \.self) { beatle in
+                        Text(beatle)
+                    }.onMove {
+                        test.beatles.move(fromOffsets: $0, toOffset: $1)
+                    }
+                }
+            }
+        }.menuBarExtraStyle(.window)
+        
+//        WindowGroup {
+//            VStack {
+//                Toggle("Toggle", isOn: $test.toggle)
+//
+//                List {
+//                    ForEach(test.beatles, id: \.self) { beatle in
+//                        Text(beatle)
+//                    }.onMove {
+//                        test.beatles.move(fromOffsets: $0, toOffset: $1)
+//                    }
+//                }
+//            }
+//        }
+    }
+}
+
+class Test: ObservableObject {
+    
+    @Published public var toggle: Bool = true {
+        didSet {
+            print(toggle)
         }
     }
+    @Published public var beatles: [String] = ["John", "Paul", "George", "Ringo"] {
+        didSet {
+            print(beatles)
+        }
+    }
+    
 }
