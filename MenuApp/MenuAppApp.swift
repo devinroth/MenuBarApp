73e14858a9f80a505efd01bfd74ec005f3e00175
Devin Roth <devin@devinrothmusic.com>
2022-12-12 13:46:19 -0800

    Initial Commit

---
+++ MenuApp/MenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuAppApp.swift
+//  MenuApp
+//
+//  Created by Devin Roth on 2022-12-12.
+//
+
+import SwiftUI
+
+@main
+struct MenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
